c0a3ada723a06fe25a29b23cc45344665d3e8bc4
jayudoye <joeludoye@gmail.com>
2024-12-07 04:44:42 +0100

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,23 @@
+import redis from 'redis';
+
+const publisher = redis.createClient();
+
+publisher.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+publisher.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send "${message}"`);
+    publisher.publish('ALX channel', message);
+  }, time);
+}
+
+publishMessage("ALX Student #1 starts course", 100);
+publishMessage("ALX Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("ALX Student #3 starts course", 400);
